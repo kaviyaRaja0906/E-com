71331c0dbe1941d0631980175712e676c766fb4e
kaviyaRaja0906 <kaviya@taptrackmedia.com>
2023-08-04 10:35:10 +0530

    env-commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const router = require('./api');
 const connectDB = require('./db');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(
   express.urlencoded({ extended: true })
